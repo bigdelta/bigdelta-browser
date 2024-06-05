7f4591973c62721ae9f664d93c6762ebf111acec
Petras Miezenas <mr.petras@gmail.com>
2024-06-05 15:46:37 +0300

    feat: track user properties

--- src/model/eventPayload.ts
+++ src/model/eventPayload.ts
@@ -1,8 +1,7 @@
+import {NestedObject} from "./nestedObject";
+
 export type Relations = { [key: string]: string };
 
-export type NestedObject = {
-  [key: string]: string | boolean | number | null | (string | boolean | number | null)[] | NestedObject;
-};
 
 export type EventPayload = {
   event_name: string;
